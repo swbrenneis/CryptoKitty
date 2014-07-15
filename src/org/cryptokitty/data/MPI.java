1924c7cbcdb97a1632b7b8f2f8a4b0f46438b60f
Steve Brenneis <swb102856@gmail.com>
2014-07-15 10:22:45 -0400

    Initial add.

---
+++ src/org/cryptokitty/data/MPI.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.cryptokitty.data;
+
+/**
+ * @author stevebrenneis
+ *
+ */
+public class MPI {
+
+	/**
+	 * 
+	 */
+	public MPI() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
