fdd3ad5cfed2b60d47bf9fea3dbef5347a85be2e
Buzz Peavis <buzzp@mail.i2p>
2014-07-15 17:44:33 -0400

    Periodic commit.

--- src/org/cryptokitty/data/MPI.java
+++ src/org/cryptokitty/data/MPI.java
@@ -3,17 +3,73 @@
  */
 package org.cryptokitty.data;
 
+import java.util.Arrays;
+
 /**
- * @author stevebrenneis
+ * @author Steve Brenneis
  *
+ * Multi-precision Integer. Similar to Java's BigInteger, but it
+ * is bit based instead of byte based. See RFC 4880, section 3.2
  */
 public class MPI {
 
-	/**
-	 * 
+	/*
+	 * Bit precision of the MPI. According to the RFC, this is a
+	 * 16 bit, unsigned quantity, so it is held here in an integer.
 	 */
-	public MPI() {
-		// TODO Auto-generated constructor stub
+	private int precision;
+
+	/*
+	 * The MPI value. Represents a big endian number, precision bits
+	 * in length.
+	 */
+	private byte[] value;
+
+	/**
+	 * Takes a precision value and a byte array value. Assumes that the
+	 * array is correctly formatted.
+	 */
+	public MPI(int precision, byte[] value) {
+		this.precision = precision;
+		this.value = value;
+	}
+
+	/**
+	 * Takes a raw byte array value (no precision octets). Calculates
+	 * precision and reformats the array.
+	 */
+	public MPI(byte[] value) {
+		// Strip leading zeros.
+		int index = 0;
+		while (index < value.length && value[index] == 0) {
+			index++;
+		}
+		this.value = Arrays.copyOfRange(value, index, value.length);
+		
+		// Calculate the precision.
+		precision = this.value.length;
+		int modbits = 8;
+		int test = this.value[0];
+		while ((test & 0x80) == 0) {
+			test = (test << 1) & 0xff;
+			modbits--;
+		}
+		precision += modbits;
+		
+	}
+
+	/*
+	 * Create an encoded octet array representing the integer.
+	 */
+	public byte[] getEncoded() {
+		// Sadly, we have to do this the hard way.
+		byte[] encoded = new byte[value.length + 2];
+		for (int i = 0; i < value.length; ++i) {
+			encoded[i+2] = value[i];
+		}
+		encoded[0] = (byte)((precision >> 8) & 0xff);
+		encoded[1] = (byte)(precision & 0xff);
+		return encoded;
 	}
 
 }
