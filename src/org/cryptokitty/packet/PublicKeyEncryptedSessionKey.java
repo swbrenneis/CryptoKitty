ad2ba6040b3be8b462efd20b8e98a35b1173e209
Buzz Peavis <buzzp@mail.i2p>
2014-07-17 18:16:48 -0400

    Periodic commit.

--- src/org/cryptokitty/packet/PublicKeyEncryptedSessionKey.java
+++ src/org/cryptokitty/packet/PublicKeyEncryptedSessionKey.java
@@ -3,10 +3,14 @@
  */
 package org.cryptokitty.packet;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.Arrays;
 
 import org.cryptokitty.data.DataException;
 import org.cryptokitty.data.KeyID;
+import org.cryptokitty.data.MPI;
+import org.cryptokitty.keys.KeyAlgorithms;
 
 /**
  * @author Steve Brenneis
@@ -15,9 +19,16 @@ import org.cryptokitty.data.KeyID;
 public class PublicKeyEncryptedSessionKey {
 
 	/*
-	 * Public key algorithm
+	 * Multiprecision integer containing the first half of
+	 * the ElGamal public key.
 	 */
-	private int algorithm;
+	private MPI elgamalMPI1;
+
+	/*
+	 * Multiprecision integer containing the second half of
+	 * the ElGamal public key.
+	 */
+	private MPI elgamalMPI2;
 
 	/*
 	 * Key ID.
@@ -30,19 +41,55 @@ public class PublicKeyEncryptedSessionKey {
 	private byte[] keypacket;
 
 	/*
-	 * Packet version number.
+	 * Public key algorithm
+	 */
+	private int pkAlgorithm;
+
+	/*
+	 * Multiprecision integer containing the RSA public key.
+	 */
+	private MPI rsaMPI;
+
+	/*
+	 * Packet version number. Should be 3.
 	 */
 	private int version;
 
 	/**
-	 * 
+	 * Constructs a PKESK from a raw packet.
 	 */
 	public PublicKeyEncryptedSessionKey(byte[] packet)
-			throws DataException {
+			throws InvalidPacketException {
+
 		version = packet[0];
-		id = new KeyID(Arrays.copyOfRange(packet, 1, 8));
-		algorithm = packet[9];
+		try {
+			id = new KeyID(Arrays.copyOfRange(packet, 1, 8));
+		}
+		catch (DataException e) {
+			throw new InvalidPacketException(e);
+		}
+		pkAlgorithm = packet[9];
 		keypacket = Arrays.copyOfRange(packet, 10, packet.length-1);
+		ByteArrayInputStream in = new ByteArrayInputStream(keypacket);
+		
+		try {
+			switch (pkAlgorithm) {
+			case KeyAlgorithms.RSA:
+			case KeyAlgorithms.RSA_ENCRYPT:
+				rsaMPI = new MPI(in);
+				break;
+			case KeyAlgorithms.ELGAMAL:
+				elgamalMPI1 = new MPI(in);
+				elgamalMPI2 = new MPI(in);
+				break;
+			default:
+				throw new InvalidPacketException("Invalid public key algorithm");
+			}
+		}
+		catch (IOException e) {
+			throw new InvalidPacketException("Invalid public key format");
+		}
+
 	}
 
 }
