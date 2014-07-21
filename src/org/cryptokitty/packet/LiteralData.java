8e35855bb3281856b84578a9ce8a886948dd5e4c
Steve Brenneis <swb102856@gmail.com>
2014-07-20 20:17:49 -0400

    Initial add.

---
+++ src/org/cryptokitty/packet/LiteralData.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package org.cryptokitty.packet;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.cryptokitty.data.DataException;
+import org.cryptokitty.data.Time;
+
+/**
+ * @author Steve Brenneis
+ *
+ * Literal data packet. Contains data to be encrypted along
+ * with format identifiers. See RFC 4880, section 5.9.
+ */
+public class LiteralData {
+
+	/*
+	 * The RFC says this is a date associated with the literal
+	 * data but makes no reference to the format. We'll assume
+	 * it is a Time object until we know otherwise.
+	 */
+	private Time date;
+
+	/*
+	 * File name.
+	 */
+	private String fileName;
+
+	/*
+	 * Format identifier.
+	 */
+	private char format;
+
+	/*
+	 * Literal data.
+	 */
+	private byte[] data;
+
+	/**
+	 * 
+	 */
+	public LiteralData(InputStream in)
+			throws InvalidPacketException {
+
+		try {
+			int f = in.read();
+			switch (f) {
+			case 'b':
+			case 't':
+			case 'u':
+				format = (char)f;
+				break;
+			default:
+				throw new InvalidPacketException("Illegal format field");
+			}
+
+			int length = in.read();
+			byte[] string = new byte[length];
+			fileName = new String(string);
+
+			try {
+				date = new Time(in);
+			}
+			catch (DataException e) {
+				throw new InvalidPacketException(e);
+			}
+
+			data = new byte[in.available()];
+			in.read(data);
+
+		}
+		catch (IOException e) {
+			throw new InvalidPacketException(e);
+		}
+	}
+
+}
