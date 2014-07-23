d702313d6af30cc7e5ab4c02bb6fce526c1a9457
Steve Brenneis <swb102856@gmail.com>
2014-07-23 08:38:58 -0400

    Initial add.

---
+++ src/org/cryptokitty/provider/CryptoKittyProvider.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.cryptokitty.provider;
+
+import java.security.Provider;
+
+/**
+ * @author Steve Brenneis
+ *
+ */
+@SuppressWarnings("serial")
+public class CryptoKittyProvider extends Provider {
+
+	/*
+	 * Provider information.
+	 */
+	private static final String INFO = "CryptoKitty Provider v0.1";
+
+	/**
+	 * 
+	 */
+	public CryptoKittyProvider() {
+		super("CryptoKitty", 0.1, INFO);
+		// TODO Auto-generated constructor stub
+	}
+
+}
