ddede1678481b9d5131d5bcd7d62506a4da5925e
Steve Brenneis <swb102856@gmail.com>
2014-07-25 08:14:15 -0400

    Initial add.

---
+++ src/org/cryptokitty/provider/BlockCipher.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package org.cryptokitty.provider;
+
+/**
+ * @author Steve Brenneis
+ *
+ * The basic block cipher interface for use in chaining
+ * mode classes.
+ */
+public interface BlockCipher {
+
+	/*
+	 * Decrypt a block of ciphertext.
+	 */
+	public byte[] decrypt(byte[] ciphertext);
+
+	/*
+	 * Encrypt a block of plaintext.
+	 */
+	public byte[] encrypt(byte[] plaintext);
+
+	/*
+	 * Get the cipher block size in bytes.
+	 */
+	public int getBlockSize();
+
+}
