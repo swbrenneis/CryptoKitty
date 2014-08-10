aedac2653ff5f9c94020c5779d5b765152140138
Steve Brenneis <swb102856@gmail.com>
2014-08-10 13:07:38 -0400

    Initial add.

---
+++ src/org/cryptokitty/provider/DecryptionException.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.cryptokitty.provider;
+
+/**
+ * @author Steve Brenneis
+ * 
+ * This is a general decryption error exception. It should be
+ * used in all decryption schemes. Only the default constructor
+ * is visible and the message is always "Decryption error."
+ * This model prevents oracles in Cipher classes.
+ *
+ */
+@SuppressWarnings("serial")
+public final class DecryptionException extends Exception {
+
+	/**
+	 * 
+	 */
+	public DecryptionException() {
+		super("Decryption error");
+	}
+
+}
