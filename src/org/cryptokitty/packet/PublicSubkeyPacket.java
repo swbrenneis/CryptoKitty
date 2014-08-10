e9181eb9c9afb45dfb1af8835f9b92647f8474d6
Steve Brenneis <swb102856@gmail.com>
2014-08-10 14:35:28 -0400

    Renamed.

---
+++ src/org/cryptokitty/packet/PublicSubkeyPacket.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.cryptokitty.packet;
+
+import java.io.InputStream;
+
+/**
+ * @author Steve Brenneis
+ *
+ * A public subkey packet is identical to a public key packet.
+ * This class is meant to differentiate the two. It may disappear.
+ */
+public class PublicSubkeyPacket extends PublicKeyPacket {
+
+	/**
+	 * @param in
+	 * @throws InvalidPacketException
+	 */
+	public PublicSubkeyPacket(InputStream in) throws InvalidPacketException {
+		super(in);
+		// TODO Auto-generated constructor stub
+	}
+
+}
