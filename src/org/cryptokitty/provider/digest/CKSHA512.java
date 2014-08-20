19972840ec7c0c8c3fad437a868897a2a4fd59c6
Steve Brenneis <swb102856@gmail.com>
2014-08-20 09:28:49 -0400

    Initial add.

---
+++ src/org/cryptokitty/provider/digest/CKSHA512.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.cryptokitty.provider.digest;
+
+/**
+ * @author Steve Brenneis
+ *
+ * SHA-512 message digest implementation
+ */
+public class CKSHA512 {
+
+	/**
+	 * 
+	 */
+	public CKSHA512() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
