ff621383610fd3bd5c24bcfc253e74e6e914fad7
Steve Brenneis <swb102856@gmail.com>
2014-09-08 13:57:40 -0400

    Initial add.

---
+++ src/org/cryptokitty/provider/signature/SHA1DSASignature.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.cryptokitty.provider.signature;
+
+/**
+ * @author Steve Brenneis
+ *
+ */
+public class SHA1DSASignature extends DSASignature {
+
+	/**
+	 * @param algorithm
+	 */
+	public SHA1DSASignature() {
+		super("SHA-1");
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/org/cryptokitty/provider/signature/SHA256DSASignature.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.cryptokitty.provider.signature;
+
+/**
+ * @author Steve Brenneis
+ *
+ */
+public class SHA256DSASignature extends DSASignature {
+
+	/**
+	 * @param algorithm
+	 */
+	public SHA256DSASignature() {
+		super("SHA-256");
+		// TODO Auto-generated constructor stub
+	}
+
+}
