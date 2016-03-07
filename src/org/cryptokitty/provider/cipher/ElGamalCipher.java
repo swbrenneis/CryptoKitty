0e6c2d259c70b98f42399d825e13e061c5c2975c
Steve Brenneis <swb102856@gmail.com>
2016-03-07 11:27:57 -0500

    Initial add.

---
+++ src/org/cryptokitty/provider/cipher/ElGamalCipher.java
@@ -0,0 +1,164 @@
+/**
+ * 
+ */
+package org.cryptokitty.provider.cipher;
+
+import java.security.AlgorithmParameters;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.AlgorithmParameterSpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.CipherSpi;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.ShortBufferException;
+
+/**
+ * @author Steve Brenneis
+ *
+ */
+public class ElGamalCipher extends CipherSpi {
+
+	/*
+	 * The cipher class.
+	 */
+	private ElGamal cipher;
+
+	/**
+	 * 
+	 */
+	public ElGamalCipher() {
+		cipher = new ElGamal();
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.crypto.CipherSpi#engineDoFinal(byte[], int, int)
+	 */
+	@Override
+	protected byte[] engineDoFinal(byte[] arg0, int arg1, int arg2)
+			throws IllegalBlockSizeException, BadPaddingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.crypto.CipherSpi#engineDoFinal(byte[], int, int, byte[], int)
+	 */
+	@Override
+	protected int engineDoFinal(byte[] arg0, int arg1, int arg2, byte[] arg3,
+			int arg4) throws ShortBufferException, IllegalBlockSizeException,
+			BadPaddingException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.crypto.CipherSpi#engineGetBlockSize()
+	 */
+	@Override
+	protected int engineGetBlockSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.crypto.CipherSpi#engineGetIV()
+	 */
+	@Override
+	protected byte[] engineGetIV() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.crypto.CipherSpi#engineGetOutputSize(int)
+	 */
+	@Override
+	protected int engineGetOutputSize(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.crypto.CipherSpi#engineGetParameters()
+	 */
+	@Override
+	protected AlgorithmParameters engineGetParameters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.crypto.CipherSpi#engineInit(int, java.security.Key, java.security.SecureRandom)
+	 */
+	@Override
+	protected void engineInit(int arg0, Key arg1, SecureRandom arg2)
+			throws InvalidKeyException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.crypto.CipherSpi#engineInit(int, java.security.Key, java.security.spec.AlgorithmParameterSpec, java.security.SecureRandom)
+	 */
+	@Override
+	protected void engineInit(int arg0, Key arg1, AlgorithmParameterSpec arg2,
+			SecureRandom arg3) throws InvalidKeyException,
+			InvalidAlgorithmParameterException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.crypto.CipherSpi#engineInit(int, java.security.Key, java.security.AlgorithmParameters, java.security.SecureRandom)
+	 */
+	@Override
+	protected void engineInit(int arg0, Key arg1, AlgorithmParameters arg2,
+			SecureRandom arg3) throws InvalidKeyException,
+			InvalidAlgorithmParameterException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.crypto.CipherSpi#engineSetMode(java.lang.String)
+	 */
+	@Override
+	protected void engineSetMode(String arg0) throws NoSuchAlgorithmException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.crypto.CipherSpi#engineSetPadding(java.lang.String)
+	 */
+	@Override
+	protected void engineSetPadding(String arg0) throws NoSuchPaddingException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.crypto.CipherSpi#engineUpdate(byte[], int, int)
+	 */
+	@Override
+	protected byte[] engineUpdate(byte[] arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.crypto.CipherSpi#engineUpdate(byte[], int, int, byte[], int)
+	 */
+	@Override
+	protected int engineUpdate(byte[] arg0, int arg1, int arg2, byte[] arg3,
+			int arg4) throws ShortBufferException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
