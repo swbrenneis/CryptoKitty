9ceb020273aef0846e26f6c43a3066bdaef7d836
Steve Brenneis <swb102856@gmail.com>
2016-03-07 11:28:34 -0500

    Initial add.

---
+++ src/org/cryptokitty/provider/signature/SHA224DSASignature.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.cryptokitty.provider.signature;
+
+/**
+ * @author Steve Brenneis
+ *
+ */
+public class SHA224DSASignature extends DSASignature {
+
+	/**
+	 * @param algorithm
+	 */
+	public SHA224DSASignature() {
+		super("SHA-224");
+		// TODO Auto-generated constructor stub
+	}
+
+}
