b04efbe460088876c889ea2d54afe8057273eef4
Steve Brenneis <swb102856@gmail.com>
2016-09-11 20:36:05 -0400

    Initial add.

---
+++ src/org/cryptokitty/provider/random/FortunaSecureRandom.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ */
+package org.cryptokitty.provider.random;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.cryptokitty.provider.SecureRandomException;
+
+/**
+ * @author stevebrenneis
+ *
+ */
+public class FortunaSecureRandom {
+
+	/**
+	 * Fortuna RNG device
+	 */
+	private static final String FORTUNAPATH = "/dev/fortuna";
+
+	/**
+	 * 
+	 */
+	public FortunaSecureRandom() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 * @param bytes
+	 * @throws SecureRandomException 
+	 */
+	public void nextBytes(byte[] bytes) throws SecureRandomException {
+
+		int length = bytes.length;
+		int offset = 0;
+		while (length > 0) {
+			byte[] rbytes = readBytes(length);
+			if (rbytes.length == 0) {
+				throw new SecureRandomException("Fortuna device read truncated");
+			}
+			System.arraycopy(rbytes, 0, bytes, offset, rbytes.length);
+			length -= rbytes.length;
+			offset += rbytes.length;
+		}
+
+	}
+
+	/**
+	 * 
+	 * @return
+	 * @throws SecureRandomException 
+	 */
+	public int nextInt() throws SecureRandomException {
+
+		byte[] bytes = new byte[4];
+		nextBytes(bytes);
+		ByteBuffer buf = ByteBuffer.wrap(bytes);
+		return buf.getInt();
+
+	}
+
+	/**
+	 * 
+	 * @return
+	 * @throws SecureRandomException 
+	 */
+	public long nextLong() throws SecureRandomException {
+
+		byte[] bytes = new byte[8];
+		nextBytes(bytes);
+		ByteBuffer buf = ByteBuffer.wrap(bytes);
+		return buf.getLong();
+
+	}
+
+	private byte[] readBytes(int count) throws SecureRandomException {
+		
+		try {
+			FileReader reader = new FileReader(FORTUNAPATH);
+			char[] cbuf = new char[count];
+			int read = reader.read(cbuf, 0, count);
+			byte[] bytes = new byte[read];
+			for (int n = 0; n < read; ++n) {
+				bytes[n] = (byte)cbuf[n];
+			}
+			reader.close();
+			return bytes;
+		}
+		catch (FileNotFoundException e) {
+			throw new SecureRandomException("Fortuna device not available");
+		}
+		catch (IOException e) {
+			throw new SecureRandomException("Fortuna read error: " + e.getMessage());
+		}
+
+	}
+
+}
