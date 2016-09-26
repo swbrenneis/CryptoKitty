262d0ac373232c9ce6ea8e8c372f4e496983b8a0
Steve Brenneis <swb102856@gmail.com>
2016-09-26 09:06:32 -0400

    Change Fortuna random to native code.

--- src/org/cryptokitty/jni/BigInteger.java
+++ src/org/cryptokitty/jni/BigInteger.java
@@ -27,17 +27,17 @@ public class BigInteger {
 	public static final BigInteger ZERO = new BigInteger(0L);
 	public static final BigInteger ONE = new BigInteger(1L);
 
-    /**
-     * The opaque pointer to the underlying C++ object.
-     */
-    private long pointer;
+	/**
+	 * The opaque pointer to the underlying C++ object.
+	 */
+	private long pointer;
 
 	/**
 	 * 
 	 */
 	public BigInteger() {
 
-        pointer = 0;
+		pointer = 0;
 
 	}
 
@@ -46,8 +46,8 @@ public class BigInteger {
 	 */
 	public BigInteger(long lValue) {
 		
-        pointer = 0;
-        initialize(lValue);
+		pointer = 0;
+		initialize(lValue);
 
 	}
 
@@ -57,16 +57,16 @@ public class BigInteger {
 	 */
 	public native int bitLength();
 
-    /**
-     * Initialize the BigInteger with a long value.
-     */
-    public native void initialize(long lValue);
+	/**
+	 * Initialize the BigInteger with a long value.
+	 */
+	public native void initialize(long lValue);
 
 	/**
 	 * 
 	 * @param exp
 	 * @param m
-	 * @return A BigInteger that is the value of m raised to exp.
+	 * @return A BigInteger that is the value of this integer raised to exp mod m.
 	 */
 	public native BigInteger modPow(BigInteger exp, BigInteger m);
 
--- src/org/cryptokitty/random/FortunaSecureRandom.java
+++ src/org/cryptokitty/random/FortunaSecureRandom.java
@@ -3,15 +3,6 @@
  */
 package org.cryptokitty.random;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
-
-import org.cryptokitty.xprovider.SecureRandomException;
-
 /**
  * @author stevebrenneis
  *
@@ -19,91 +10,47 @@ import org.cryptokitty.xprovider.SecureRandomException;
 public class FortunaSecureRandom extends SecureRandomWrapper implements SecureRandom {
 
 	/**
+	 * Load the CryptoKitty-C binary.
+	 */
+	static {
+		System.loadLibrary("cryptokitty");
+	}
+
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -3455700191950379176L;
 
 	/**
-	 * Fortuna RNG device
+	 * Opaque pointer to the underlying C++ object.
 	 */
-	private static final String FORTUNAPATH = "/dev/fortuna";
+	private long pointer;
 
 	/**
 	 * 
 	 */
 	public FortunaSecureRandom() {
-		// TODO Auto-generated constructor stub
+
+		pointer = 0;
+
 	}
 
 	/**
 	 * 
 	 * @param bytes
 	 */
-	public void nextBytes(byte[] bytes) {
-
-		int length = bytes.length;
-		int offset = 0;
-		try {
-			while (length > 0) {
-				byte[] rbytes = readBytes(length);
-				if (rbytes.length != 0) {
-					System.arraycopy(rbytes, 0, bytes, offset, rbytes.length);
-					length -= rbytes.length;
-					offset += rbytes.length;
-				}
-			}
-		}
-		catch (SecureRandomException e) {
-			// No clue what to do with this.
-			System.err.println(e.getMessage());
-		}
-
-	}
+	public native void nextBytes(byte[] bytes);
 
 	/**
 	 * 
 	 * @return
 	 */
-	public int nextInt() {
-
-		byte[] bytes = new byte[4];
-		nextBytes(bytes);
-		ByteBuffer buf = ByteBuffer.wrap(bytes);
-		return buf.getInt();
-
-	}
+	public native int nextInt();
 
 	/**
 	 * 
 	 * @return
 	 */
-	public long nextLong() {
-
-		byte[] bytes = new byte[8];
-		nextBytes(bytes);
-		ByteBuffer buf = ByteBuffer.wrap(bytes);
-		return buf.getLong();
-
-	}
-
-	private byte[] readBytes(int count) throws SecureRandomException {
-		
-		try {
-			File file = new File(FORTUNAPATH);
-			file.setReadOnly();
-			InputStream in = new FileInputStream(file);
-			byte[] bytes = new byte[count];
-			in.read(bytes);
-			in.close();
-			return bytes;
-		}
-		catch (FileNotFoundException e) {
-			throw new SecureRandomException("Fortuna device not available");
-		}
-		catch (IOException e) {
-			throw new SecureRandomException("Fortuna read error: " + e.getMessage());
-		}
-
-	}
+	public native long nextLong();
 
 }
