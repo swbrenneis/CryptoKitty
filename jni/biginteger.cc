52815277b005e5e3286ba4f0401fbe269d9adc05
Steve Brenneis <steve.brenneis@secomm.org>
2016-09-26 21:48:00 -0400

    Finished coding and testing.

--- jni/biginteger.cc
+++ jni/biginteger.cc
@@ -1,5 +1,7 @@
 #include "org_cryptokitty_jni_BigInteger.h"
 #include <CryptoKitty-C/data/BigInteger.h>
+#include <CryptoKitty-C/random/FortunaSecureRandom.h>
+#include <sstream>
 
 /**
  * Retrieve the opaque pointer reference.
@@ -8,33 +10,83 @@ static CK::BigInteger *getReference(JNIEnv *env, jobject thisObj) {
 
     jclass thisClass = env->GetObjectClass(thisObj);
     // TODO Throw an exception if null.
-    jfieldID fieldId = env->GetFieldID(thisClass, "pointer", "L");
+    jfieldID fieldId = env->GetFieldID(thisClass, "pointer", "J");
     jlong pointer = env->GetLongField(thisObj, fieldId);
-    return reinterpret_cast<CK::BigInteger*>(pointer);
+    if (pointer != 0) {
+        return reinterpret_cast<CK::BigInteger*>(pointer);
+    }
+    else {
+        CK::BigInteger *ref = new CK::BigInteger;
+        pointer = reinterpret_cast<jlong>(ref);
+        env->SetLongField(thisObj, fieldId, pointer);
+        return ref;
+    }
+
+}
+
+JNIEXPORT jint JNICALL
+Java_org_cryptokitty_jni_BigInteger_bitLength (JNIEnv *env, jobject thisObj) {
+
+    CK::BigInteger *ref = getReference(env, thisObj);
+    return ref->bitLength();
 
 }
 
 JNIEXPORT void JNICALL
-Java_org_cryptokitty_jni_BigInteger_initialize__ (JNIEnv *env, jobject thisObj) {
+Java_org_cryptokitty_jni_BigInteger_initialize (JNIEnv *env, jobject thisObj, jlong lValue) {
 
     jclass thisClass = env->GetObjectClass(thisObj);
     // TODO Throw an exception if null.
-    jfieldID fieldId = env->GetFieldID(thisClass, "pointer", "L");
-    jlong pointer = env->GetLongField(thisObj, fieldId);
-    pointer = reinterpret_cast<jlong>(new CK::BigInteger);
-    env->SetLongField(thisObj, fieldId, pointer);
-
-}
-
-JNIEXPORT void JNICALL
-Java_org_cryptokitty_jni_BigInteger_initialize__J (JNIEnv *env, jobject thisObj, jlong lValue) {
-
-    jclass thisClass = env->GetObjectClass(thisObj);
-    // TODO Throw an exception if null.
-    jfieldID fieldId = env->GetFieldID(thisClass, "pointer", "L");
+    jfieldID fieldId = env->GetFieldID(thisClass, "pointer", "J");
     jlong pointer = env->GetLongField(thisObj, fieldId);
     pointer = reinterpret_cast<jlong>(new CK::BigInteger(lValue));
     env->SetLongField(thisObj, fieldId, pointer);
+
+}
+
+JNIEXPORT jobject JNICALL
+Java_org_cryptokitty_jni_BigInteger_modPow (JNIEnv *env, jobject thisObj, jobject expObj,
+                                                                            jobject modObj) {
+
+    CK::BigInteger *me = getReference(env, thisObj);
+    CK::BigInteger *exp = getReference(env, expObj);
+    CK::BigInteger *mod = getReference(env, modObj);
+    CK::BigInteger *answer = new CK::BigInteger(me->modPow(*exp, *mod));
+
+    jclass biClass = env->FindClass("org/cryptokitty/jni/BigInteger");
+    jmethodID biInitId = env->GetMethodID(biClass, "<init>", "()V");
+    jobject newBI = env->NewObject(biClass, biInitId);
+    jfieldID fieldId = env->GetFieldID(biClass, "pointer", "J");
+    jlong pointer = reinterpret_cast<jlong>(answer);
+    env->SetLongField(newBI, fieldId, pointer);
+
+    return newBI;
+
+}
+
+JNIEXPORT jobject JNICALL
+Java_org_cryptokitty_jni_BigInteger_probablePrime (JNIEnv *env, jclass, jint bitsize) {
+
+    CK::FortunaSecureRandom rnd;
+    CK::BigInteger *bi = new CK::BigInteger(bitsize, false, rnd);
+    jclass biClass = env->FindClass("org/cryptokitty/jni/BigInteger");
+    jmethodID biInitId = env->GetMethodID(biClass, "<init>", "()V");
+    jobject newBI = env->NewObject(biClass, biInitId);
+    jfieldID fieldId = env->GetFieldID(biClass, "pointer", "J");
+    jlong pointer = reinterpret_cast<jlong>(bi);
+    env->SetLongField(newBI, fieldId, pointer);
+
+    return newBI;
+
+}
+
+JNIEXPORT jstring JNICALL
+Java_org_cryptokitty_jni_BigInteger_toString (JNIEnv *env, jobject thisObj) {
+
+    CK::BigInteger *ref = getReference(env, thisObj);
+    std::ostringstream str;
+    str << *ref;
+    return env->NewStringUTF(str.str().c_str());
 
 }
 
