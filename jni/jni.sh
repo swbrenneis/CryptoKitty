bd024570f8c6d1edba9e4e1201e3e50e472eb939
Steve Brenneis <steve.brenneis@secomm.org>
2016-09-29 13:12:15 +0000

    Added HMAC, refactored ByteArrayHolder.

--- jni/jni.sh
+++ jni/jni.sh
@@ -4,6 +4,7 @@ javah -cp CryptoKitty.jar org.cryptokitty.jni.BigInteger
 javah -cp CryptoKitty.jar org.cryptokitty.random.FortunaSecureRandom
 javah -cp CryptoKitty.jar org.cryptokitty.cipher.AES
 javah -cp CryptoKitty.jar org.cryptokitty.modes.GCM
+javah -cp CryptoKitty.jar org.cryptokitty.mac.HMAC
 
 make
 
