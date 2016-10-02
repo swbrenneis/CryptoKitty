76a2ab1e416fe049313e20e7ee01d4fd533c1aa0
Steve Brenneis <steve.brenneis@secomm.org>
2016-10-02 12:11:18 +0000

    Development.

--- jni/jni.sh
+++ jni/jni.sh
@@ -5,6 +5,7 @@ javah -cp CryptoKitty.jar org.cryptokitty.random.FortunaSecureRandom
 javah -cp CryptoKitty.jar org.cryptokitty.cipher.AES
 javah -cp CryptoKitty.jar org.cryptokitty.modes.GCM
 javah -cp CryptoKitty.jar org.cryptokitty.mac.HMAC
+javah -cp CryptoKitty.jar org.cryptokitty.keys.RSAKeyPairGenerator
 
 make
 
