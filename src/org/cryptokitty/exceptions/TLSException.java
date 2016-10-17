1dfd6cfacab8225ece7143d469fe85f630f227f2
Buzz Peavis <buzzp@mail.i2p>
2016-10-17 14:12:53 -0400

    Initial add.

---
+++ src/org/cryptokitty/exceptions/TLSException.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package org.cryptokitty.exceptions;
+
+/**
+ * @author stevebrenneis
+ *
+ */
+public class TLSException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1516342594996166252L;
+
+	/**
+	 * 
+	 */
+	public TLSException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public TLSException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public TLSException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public TLSException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public TLSException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
