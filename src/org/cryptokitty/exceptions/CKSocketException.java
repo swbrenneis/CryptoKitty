d38a86782829d712f19df15618f714431050cc1d
Steve Brenneis <swb102856@gmail.com>
2016-10-20 13:07:39 -0400

    Initial add.

---
+++ src/org/cryptokitty/exceptions/CKSocketException.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package org.cryptokitty.exceptions;
+
+/**
+ * @author stevebrenneis
+ *
+ */
+public class CKSocketException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3975675645831832047L;
+
+	/**
+	 * 
+	 */
+	public CKSocketException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public CKSocketException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public CKSocketException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public CKSocketException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public CKSocketException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
