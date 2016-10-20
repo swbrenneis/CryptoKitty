627fed2c944e65ecf0ba4ec2200a11d98b398586
Steve Brenneis <steve.brenneis@secomm.org>
2016-10-20 16:03:02 -0400

    Adding TLS and Berkeley socket.

--- jni/jni.sh
+++ jni/jni.sh
@@ -1,11 +1,14 @@
 #! /bin/sh
 
 javah -cp CryptoKitty.jar org.cryptokitty.jni.BigInteger
+javah -cp CryptoKitty.jar org.cryptokitty.jni.BerkeleySocketImpl
 javah -cp CryptoKitty.jar org.cryptokitty.random.FortunaSecureRandom
 javah -cp CryptoKitty.jar org.cryptokitty.cipher.AES
 javah -cp CryptoKitty.jar org.cryptokitty.modes.GCM
 javah -cp CryptoKitty.jar org.cryptokitty.mac.HMAC
 javah -cp CryptoKitty.jar org.cryptokitty.keys.RSAKeyPairGenerator
+javah -cp CryptoKitty.jar org.cryptokitty.tls.TLSCredentials
+javah -cp CryptoKitty.jar org.cryptokitty.tls.TLSSession
 
 make
 
